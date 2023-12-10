e0f5398ee0712a8ccdb69ecac7512b3c415632be
nightpacifist <nightpacifist@gmail.com>
2023-12-10 17:42:38 +0200

    init

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Привіт, світ!'
+
+
+@app.route('/en')
+def other():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

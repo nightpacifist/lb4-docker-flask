80f980298828d568452b9e3b898cdc5a6b18184d
nightpacifist <nightpacifist@gmail.com>
2025-01-21 12:20:08 +0200

    new fix

--- app/main.py
+++ app/main.py
@@ -11,6 +11,10 @@ def hello_world():
 def other():
     return "Hello, World!"
 
+@app.route('/en2')
+def other():
+    return "Hello, World!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
